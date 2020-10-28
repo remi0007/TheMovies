59603b8cebbd50459e970fe357467ede2ca1081b
Remi Douah <remidouah@gmail.com>
2020-10-27 19:57:41 -0500

    Movie night in Amsterdam

---
+++ TheMovies/Movies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMovies
+{
+    class Movies
+
+      
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movies(string aTitle, string aDirector, string aRating)
+        {
+
+            title = aTitle;
+            director = aDirector;
+            rating = aRating;
+
+        }
+
+        public string Rating
+        {
+
+            get { return rating; }
+            set 
+            
+            { 
+            if (value == "G" || value == "PG" || value == "PG-13" || value == "R"  || value == "NR")
+                {
+
+                    rating = value;
+                }
+            else
+                {
+
+                    rating = "NR";
+                }
+            }
+
+
+        }
+    }
+}
---
+++ TheMovies/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace TheMovies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Movies avengers = new Movies("The Avengers", "Joss Whedon", "PG-13");
+                Movies shrek = new Movies("Shrek", "Adam", "PG");
+
+            shrek.Rating = "R";
+            Console.WriteLine(avengers.Rating);
+
+            Console.ReadLine();
+        }
+    }
+}
